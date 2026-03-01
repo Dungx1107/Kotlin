f94855c908787c2b6b40a53a3a8ab8bd5d05e0fd
Dungx1107 <nguyenxuandung457@gmail.com>
2026-03-01 21:43:15 +0700

    góc phần tư của Composable

--- thuchanhcompose/app/src/main/java/com/example/thuchanhcompose/MainActivity.kt
+++ thuchanhcompose/app/src/main/java/com/example/thuchanhcompose/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +33,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ThucHanhComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+//                    Greeting(
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                    GocPhanTuTrongComposable(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -72,10 +80,137 @@ fun Greeting(modifier: Modifier = Modifier) {
     }
 }
 
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ThucHanhComposeTheme {
+//        Greeting()
+//    }
+//}
+
+@Composable
+fun GocPhanTuTrongComposable(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize() //phải chiến hết chiều dài của Row hoặc thành phần chứa nó thì mới căn được chữ vào giữa
+                    .background(
+                        color = Color(0xFFEADDFF)
+                    )
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.text41),
+                    textAlign = TextAlign.Justify,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally),
+                )
+                Text(
+                    text = stringResource(R.string.text42),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .background(
+                        color = Color(0xFFD0BCFF)
+                    )
+                    .weight(1f)
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.text43),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold,
+
+                    )
+                Text(
+                    text = stringResource(R.string.text44),
+                    textAlign = TextAlign.Justify
+
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize(),
+
+            ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(
+                        color = Color(0xFFB69DF8)
+                    )
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center
+
+            ) {
+                Text(
+                    text = stringResource(R.string.text45),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold,
+
+                    )
+                Text(
+                    text = stringResource(R.string.text46),
+                    textAlign = TextAlign.Justify
+
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(
+                        color = Color(0xFFF6EDFF)
+                    )
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center
+
+            ) {
+                Text(
+                    text = stringResource(R.string.text47),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold,
+
+                    )
+                Text(
+                    text = stringResource(R.string.text48),
+                    textAlign = TextAlign.Justify
+
+                )
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview1() {
     ThucHanhComposeTheme {
-        Greeting()
+        GocPhanTuTrongComposable()
     }
 }

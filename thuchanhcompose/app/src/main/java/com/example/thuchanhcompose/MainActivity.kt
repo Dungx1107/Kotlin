60ef26733985a6b7de602872bfe02478aa356dd3
Dungx1107 <nguyenxuandung457@gmail.com>
2026-03-01 16:49:12 +0700

    bài viết về compose

--- thuchanhcompose/app/src/main/java/com/example/thuchanhcompose/MainActivity.kt
+++ thuchanhcompose/app/src/main/java/com/example/thuchanhcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,7 +15,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.thuchanhcompose.ui.theme.ThucHanhComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,22 +39,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    Column() {
-        image = painterResource(R.drawable.bg_compose_background)
-        Image() {
-            
-        }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier,
+    ) {
+        val image = painterResource(R.drawable.bg_compose_background)
+        Image(
+            painter = image,
+            contentDescription = null
+        )
         Text(
             text = stringResource(R.string.text1),
-            modifier = modifier
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+//                .fillMaxSize()
         )
         Text(
             text = stringResource(R.string.text2),
-            modifier = modifier
+            modifier = Modifier
+                .padding(16.dp, 0.dp, 16.dp),
+//                .fillMaxSize(),
+            textAlign = TextAlign.Justify
         )
         Text(
             text = stringResource(R.string.text3),
-            modifier = modifier
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp, 16.dp, 16.dp, 16.dp)
+
         )
     }
 }

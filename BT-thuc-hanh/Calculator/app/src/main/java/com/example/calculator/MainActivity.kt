e425a20a7285ea092ab21ea057ef0fdce8d3294b
Dungx1107 <nguyenxuandung457@gmail.com>
2026-03-13 13:52:28 +0700

    calculator

--- BT-thuc-hanh/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ BT-thuc-hanh/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -7,23 +7,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,24 +49,68 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Calculator(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Calculator()
             }
         }
     }
 }
 
 @Composable
-fun Calculator(modifier: Modifier = Modifier) {
-    Column() {
+fun Calculator() {
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .widthIn(max = 350.dp)   // giới hạn chiều rộng
+            .systemBarsPadding()//Né thanh điều hướng và thanh trạng thái
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        var text1 by remember { mutableStateOf("") } // bieu thuc
+        var text2 by remember { mutableStateOf("") } // ket qua
 
-//        // nhap phep tinh
-//        TextField(
-//            value = expres
-//        )
+        val handleButtonClick: (String) -> Unit = { input ->
+            val (newText1, newText2) = CalculatorLogic(input, text1, text2)
+            text1 = newText1
+            text2 = newText2
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            // nhap phep tinh
+            TextField(
+                value = text1,
+                onValueChange = { text1 = it },
+                label = { Text("input") },
+                modifier = Modifier
+                    .weight(2f)
+                    .fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White
+                )
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = text2,
+                onValueChange = { text2 = it },
+                label = { Text("result") },
+                modifier = Modifier
+                    .weight(2f)
+                    .fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White
+                )
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
 
         CalculatorRow(
             "C", "del", "%", "/",
@@ -62,16 +118,18 @@ fun Calculator(modifier: Modifier = Modifier) {
             Color(0xFFC3D4BD),// xanh nhat
             Color(0xFFC3D4BD),
             Color(0xFFB38A2E)// vang
+            , handleButtonClick
         )
-        Spacer(modifier = Modifier.height(16.dp))
+
         CalculatorRow(
             "7", "8", "9", "x",
             Color(0xFFC3D4BD), //
             Color(0xFFC3D4BD),// xanh nhat
             Color(0xFFC3D4BD),
             Color(0xFFB38A2E)// vang
+            , handleButtonClick
+
         )
-        Spacer(modifier = Modifier.height(16.dp))
 
         CalculatorRow(
             "4", "5", "6", "-",
@@ -79,8 +137,9 @@ fun Calculator(modifier: Modifier = Modifier) {
             Color(0xFFC3D4BD),// xanh nhat
             Color(0xFFC3D4BD),
             Color(0xFFB38A2E)// vang
+            , handleButtonClick
+
         )
-        Spacer(modifier = Modifier.height(16.dp))
 
         CalculatorRow(
             "1", "2", "3", "+",
@@ -88,9 +147,9 @@ fun Calculator(modifier: Modifier = Modifier) {
             Color(0xFFC3D4BD),// xanh nhat
             Color(0xFFC3D4BD),
             Color(0xFFB38A2E)// vang
-        )
+            , handleButtonClick
 
-        Spacer(modifier = Modifier.height(16.dp))
+        )
 
         CalculatorRow(
             "00", "0", ".", "=",
@@ -98,6 +157,8 @@ fun Calculator(modifier: Modifier = Modifier) {
             Color(0xFFC3D4BD),// xanh nhat
             Color(0xFFC3D4BD),
             Color(0xFF2E7D32)// xanh dam
+            , handleButtonClick
+
         )
 
 
@@ -113,36 +174,42 @@ fun CalculatorRow(
     color1: Color,
     color2: Color,
     color3: Color,
-    color4: Color
+    color4: Color,
+    onBtnClick: (String) -> Unit // Nhận giá trị nút vừa bấm
 ) {
     Row(
-        horizontalArrangement = Arrangement.Center,
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
         modifier = Modifier.fillMaxWidth()
     ) {
-        CalculatorButton(text1, color1)
-
-        Spacer(modifier = Modifier.width(16.dp))
-
-        CalculatorButton(text2, color2)
-
-        Spacer(modifier = Modifier.width(16.dp))
-
-        CalculatorButton(text3, color3)
-
-        Spacer(modifier = Modifier.width(16.dp))
-
-        CalculatorButton(text4, color4)
+        CalculatorButton(text1, color1, Modifier.weight(1f)) {
+            onBtnClick(text1)
+        }
+        CalculatorButton(text2, color2, Modifier.weight(1f)) {
+            onBtnClick(text2)
+        }
+        CalculatorButton(text3, color3, Modifier.weight(1f)) {
+            onBtnClick(text3)
+        }
+        CalculatorButton(text4, color4, Modifier.weight(1f)) {
+            onBtnClick(text4)
+        }
     }
 }
 
 @Composable
 fun CalculatorButton(
     text: String,
-    color: Color
+    color: Color,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
 ) {
     Button(
-        onClick = {},
-        modifier = Modifier.size(80.dp),
+        onClick = onClick,
+        modifier = Modifier
+            .size(50.dp)
+            .aspectRatio(1f) // Tạo nút hình tròn hoàn hảo
+        ,
+        contentPadding = PaddingValues(0.dp), // Căn giữa chữ
         shape = CircleShape,
         colors = ButtonDefaults.buttonColors(
             containerColor = color
@@ -152,9 +219,62 @@ fun CalculatorButton(
             text = text,
             color = Color.Black,
             fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            maxLines = 1,
+            softWrap = false
+
         )
     }
+}
+
+fun CalculatorLogic(
+    input: String,
+    currentText1: String,
+    currentText2: String
+): Pair<String, String> {
+    var nextText1 = currentText1
+    var nextText2 = currentText2
+
+    when (input) {
+        "C" -> {
+            nextText1 = ""
+            nextText2 = ""
+        }
+
+        "del" -> {
+            if (!nextText1.isEmpty()) {
+                nextText1.dropLast(1)
+            }
+        }
+
+        "=" -> {
+            try {
+                val expressionStr = nextText1.replace("x", "*")
+                val result = CalculatorByString(expressionStr)
+                if (result % 1 == 0.0) {
+                    nextText2 = result.toInt().toString()
+                } else {
+                    nextText2 = result.toString()
+                }
+            } catch (e: Exception) {
+                nextText2 = "Error"
+            }
+        }
+
+        else -> {
+            nextText1 += input
+        }
+    }
+    return Pair(nextText1, nextText2)
+}
+
+fun CalculatorByString(input: String): Double {
+    val expressionStr = input.replace("x", "*")
+    return net.objecthunter.exp4j
+        .ExpressionBuilder(expressionStr)
+        .build().evaluate()
+
+
 }
 
 @Preview(showBackground = true)

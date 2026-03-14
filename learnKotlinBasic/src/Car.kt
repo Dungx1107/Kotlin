9add4339400c1f8bff1f75dd311bac152cba6fa5
Dungx1107 <nguyenxuandung457@gmail.com>
2026-03-14 12:34:35 +0700

    Dùng hàm phạm vi để truy cập vào các phương thức và thuộc tính của lớp

--- learnKotlinBasic/src/Car.kt
+++ learnKotlinBasic/src/Car.kt
@@ -3,7 +3,19 @@ class Car : Vehical {
         get() = 4
     override val name: String
         get() = "Car"
+    var nameChu: String = ""
+    var giaTien: String = ""
 
+    constructor(name: String, giaTien: String) {
+        this.nameChu = name
+        this.giaTien = giaTien
+    }
+    fun desCar(){
+        print("gia tin la ${giaTien}" )
+    }
+    fun desChu(){
+        print("ten cua chu ${nameChu}")
+    }
 //    override fun go() {
 //        super.go()
 //        println("$name go")
--- learnKotlinBasic/src/Main.kt
+++ learnKotlinBasic/src/Main.kt
@@ -1,9 +1,27 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-   var car = Car()
-    println("Car is $car")
-    println(car.name)
+//   var car = Car()
+//    println("Car is $car")
+//    println(car.name)
+//
+//    Car().go()
 
-    Car().go()
+    var car1 = Car("Dung", "1 ti")
+    var car2 = Car("Dung1", "2 ti")
+    var car3 = Car("Dung2", "3 ti")
+
+    println(car1.giaTien)
+    println(car1.nameChu)
+
+    car1.let {
+        println(it.giaTien)
+        println(it.nameChu)
+    }
+
+    car1.apply {
+        desCar()
+        desChu()
+    }
+
 }
